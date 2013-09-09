d0a09ea89d2d95c5844eaf52089b58ae55431dbe
Sheniff <sheniff@gmail.com>
2013-09-09 00:50:27 -0700

    WIP thinkster tuto

--- config/routes.js
+++ config/routes.js
@@ -12,9 +12,18 @@ module.exports = function (app, passport, auth) {
   app.post('/users/session', passport.authenticate('local', {failureRedirect: '/signin', failureFlash: 'Invalid email or password.'}), users.session)
   app.get('/users/me', users.me)
   app.get('/users/:userId', users.show)
-  
+
   app.param('userId', users.user)
-  
+
+  var leagues = require('../app/controllers/leagues')
+  app.get('/leagues', leagues.all)
+  app.post('/leagues', auth.requiresLogin, leagues.create)
+  app.get('/leagues/:leagueId', leagues.show)
+  app.put('/leagues/:leagueId', auth.requiresLogin, leagues.update)
+  app.del('/leagues/:leagueId', auth.requiresLogin, leagues.destroy)
+
+  app.param('leagueId', leagues.league)
+
   // home route
   var index = require('../app/controllers/index')
   app.get('/', index.render)
--- public/js/config.js
+++ public/js/config.js
@@ -1,7 +1,34 @@
 //Setting up route
 window.app.config(['$routeProvider', function($routeProvider) {
 	$routeProvider
-	.when('/', { templateUrl: 'views/index.html' })
+  .when('/',
+  {
+    templateUrl: 'views/index.html'
+  })
+  .when('/nflteams',
+  {
+    templateUrl: 'views/nfl/list.html'
+  })
+  .when('/nflteams/:nflTeamId',
+  {
+    templateUrl: 'views/nfl/view.html'
+  })
+  .when('/leagues',
+  {
+    templateUrl: 'views/leagues/list.html'
+  })
+  .when('/leagues/create',
+  {
+    templateUrl: 'views/leagues/create.html'
+  })
+  .when('/leagues/:leagueId/edit',
+  {
+    templateUrl: 'views/leagues/edit.html'
+  })
+  .when('/leagues/:leagueId',
+  {
+    templateUrl: 'views/leagues/view.html'
+  })
 	.otherwise({redirectTo: '/'});
 }]);
 
